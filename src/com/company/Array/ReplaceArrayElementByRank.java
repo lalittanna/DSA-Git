4f89aa788654943e86593a9373a3c1985b83aa18
lalit <lalittanna77@gmail.com>
2022-01-07 10:01:46 +0000

    Added question in comments

--- src/com/company/Array/ReplaceArrayElementByRank.java
+++ src/com/company/Array/ReplaceArrayElementByRank.java
@@ -3,6 +3,17 @@ package com.company.Array;
 import java.util.Map;
 import java.util.TreeMap;
 
+/*
+
+Given an array of distinct integers, replace each array element by its corresponding rank in the array. The minimum array element has the rank 1; the second minimum element has a rank of 2, and so on.
+
+Input : [10, 8, 15, 12, 6, 20, 1]
+Output: [4, 3, 6, 5, 2, 7, 1]
+
+Input : [0, 1, -1]
+Output: [2, 3, 1]
+
+*/
 public class ReplaceArrayElementByRank {
     public static void transform(int[] nums)
     {
