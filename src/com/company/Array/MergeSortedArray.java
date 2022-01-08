0fc87f68d58ee44ef7ee7bbcb5ea44b1c2461703
lalit <lalittanna77@gmail.com>
2022-01-08 16:56:23 +0000

    Added 1 more question

---
+++ src/com/company/Array/MergeSortedArray.java
@@ -0,0 +1,35 @@
+package com.company.Array;
+
+/*
+
+Given two integer arrays, each of which is sorted in increasing order, merge them into a single array in increasing order, and return it.
+
+Input: X = [1, 3, 5, 7], Y = [2, 4, 6]
+Output: [1, 2, 3, 4, 5, 6, 7]
+
+*/
+public class MergeSortedArray {
+    public static int[] merge(int[] X, int[] Y)
+    {
+        int[] mergedArray = new int[X.length + Y.length];
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < X.length && j < Y.length){
+            if (X[i] < Y[j]){
+                mergedArray[k++] = X[i];
+                i++;
+            } else {
+                mergedArray[k++] = Y[j];
+                j++;
+            }
+        }
+
+        while (i < X.length)
+            mergedArray[k++] = X[i++];
+        while (j < Y.length)
+            mergedArray[k++] = Y[j++];
+
+        return mergedArray;
+    }
+}
