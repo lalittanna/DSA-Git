e7ef3b271ade08d3b2e160ab0ae9c7b9c7a36706
lalit <lalittanna77@gmail.com>
2022-01-11 23:23:46 +0000

    Added 1 more question

---
+++ src/com/company/SumOfSquareNumbers.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class SumOfSquareNumbers {
+    // Sum of square of first 'n' natural numbers.
+    public static int sum(int n){
+        return n * (n + 1) * (2 * n + 1) / 6;
+    }
+}
