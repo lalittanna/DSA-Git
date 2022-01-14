9fb700dc91469ce85de92c5807dc3d1a8e9ef7b5
lalit <lalittanna77@gmail.com>
2022-01-14 19:21:18 +0000

    Added question comment.

--- src/com/company/Array/MaximumBinaryContinuousSequence.java
+++ src/com/company/Array/MaximumBinaryContinuousSequence.java
@@ -1,5 +1,32 @@
 package com.company.Array;
 
+/*
+
+Given a binary array and a positive integer `k`, return the indices of the maximum sequence of continuous 1’s that can be formed by replacing at most `k` zeroes by ones.
+
+• The solution should return a pair of the starting and the ending index of the maximum sequence.
+• For invalid inputs, the solution should return null.
+• In case multiple sequence of continuous 1’s of maximum length exists, the solution can return any one of them.
+
+Input : nums[] = [1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0], k = 0
+Output: (6, 9)
+Explanation: The longest sequence of continuous 1’s is formed by index 6 to 9.
+
+Input : nums[] = [1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0], k = 1
+Output: (3, 9)
+Explanation: The longest sequence of continuous 1’s is formed by index 3 to 9 on replacing zero at index 5.
+
+Input : nums[] = [1, 1, 1, 1, 1], k = 1
+Output: (0, 4)
+
+Input : nums[] = [1, 0, 1, 1, 0, 0, 1, 1, 0, 1], k = 1
+Output: (0, 3) or (6, 9)
+
+Input : nums[] = [], k = 1
+Output: null
+
+*/
+
 public class MaximumBinaryContinuousSequence {
     private class  Pair<E, V> {
         public static int[] of(int U, int V){
