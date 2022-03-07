cac2bb5eb941be51642184418a74fca8e99b448f
lalit <lalittanna77@gmail.com>
2022-03-07 08:21:29 +0000

    Added 1 more question.

---
+++ src/com/company/Backtracking/Combinations5.java
@@ -0,0 +1,48 @@
+package com.company.Backtracking;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Combinations5 {
+    /*
+
+    Given a positive integer `n`, find all combinations of numbers between 1 and `n` having sum `n`.
+
+    Input : n = 4
+    Output: {[4], [1, 3], [2, 2], [1, 1, 2], [1, 1, 1, 1]}
+
+    Input : n = 5
+    Output: {[5], [1, 4], [2, 3], [1, 1, 3], [1, 2, 2], [1, 1, 1, 2], [1, 1, 1, 1, 1]}
+
+    The solution should return a set containing all the distinct combinations in increasing order.
+
+    */
+    public static Set<List<Integer>> findCombinations(int n){
+        Set<List<Integer>> combinations = new HashSet<>();
+
+        combinations(combinations, new ArrayList<>(), 1, n);
+
+        return combinations;
+    }
+
+    public static void combinations(Set<List<Integer>> combinations, List<Integer> list, int j, int n){
+        if (n == 0){
+            List<Integer> combination = new ArrayList<>(list);
+            combinations.add(combination);
+            return;
+        }
+
+        for (int i = j; i <= n; i++){
+            if (n - i >= 0){
+                list.add(i);
+
+                combinations(combinations, list, i, n - i);
+
+                list.remove(list.size() - 1);
+            } else
+                break;
+        }
+    }
+}
